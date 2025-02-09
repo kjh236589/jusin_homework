3d3080bff9b89bc6d0064f4aadc4f84c6d3fd28b
IBulan <40731551+IBulan@users.noreply.github.com>
2025-02-09 23:15:50 +0900

    수업 내용 동기화

--- 250207/메모리 함수.cpp
+++ 250207/메모리 함수.cpp
@@ -1,30 +1,36 @@
-#include<iostream>
-#include<Windows.h>
+#include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
 	// 메모리 함수 : 메모리 대 메모리로 동작을 수행하는 함수
 
-	// int iArray[5] = { 1,2,3,4,5 };
+	// int		iArray[5] = { 1, 2, 3, 4, 5 };
 	// 
 	// ZeroMemory
 	// 
-	// memset : 메모리 초기화 함수(초기화를 수행할 메모리 공간의 주소, 바이트 단위로 채울 값, 
-	// 
+	// // memset : 메모리 초기화 함수(초기화를 수행할 메모리 공간의 주소, 바이트 단위로 채울 값, 초기화를 수행할 메모리의 크기)
 	// memset(iArray, 0, sizeof(iArray));
 	// 
-	// 
-	// for (int i = 0; i < 5; ++i) {
+	// for (int i = 0; i < 5; ++i)
+	// {
 	// 	cout << iArray[i] << endl;
 	// }
+
+	// int		iArray[5] = { 1, 2, 3, 4, 5 };
+	// int		iTemp[5] = {};
 	// 
-	// int iArray[5] = { 1,2,3,4,5 };
-	// int iTemp[10] = { 10,20,30,40,50,60,70 };
+	// //memcpy(iTemp, iArray, sizeof(iTemp));
+	// memmove(iTemp, iArray, sizeof(iTemp));
 	// 
-	// // memcpy(iTemp, iArray, sizeof(iArray));
-	// memmove(iTemp, iArray, sizeof(iArray));
-	// for (int i = 0; i < 10; ++i) {
+	// for (int i = 0; i < 5; ++i)
+	// {
 	// 	cout << iTemp[i] << endl;
 	// }
+
+
+
+	return 0;
 }
--- 250207/전처리기.cpp
+++ 250207/전처리기.cpp
@@ -1,48 +1,50 @@
-#include<iostream>
+#include <iostream>
 
 // 매크로 : 자주 사용하는 코드 블럭
-//	상수 매크로
 
-#define	PI	3.14f
+// 상수 매크로(단순 치환)
 
-#define MIN_STR 32
-#define MAX_STR 123
+#define		PI		3.14f
 
-//	함수 매크로
-// 1. 동작 속도가 빠름(코드가 그 삽입 친환(삽입))
+#define		MIN_STR		32
+#define		MAX_STR		128
+
+// 함수 매크로
+// 1. 동작 속도가 빠름(코드가 그 삽입 치환(삽입))
 // 2. 자료형에 따른 제한이 없음
-// 
-// 단점
-// 함수 매크로 안의 코드가 일어지면 가독성이 떨어짐
-#define	SQUARE(X)	(X)\
-*\
-(X)
 
-#define SAFE_DELETE(p)	if(p){\
-delete p;\
-p = nullptr;\
-}
+// 단점 
+// 함수 매크로 안의 코드가 길어지면 가독성이 떨어짐
+#define		SQUARE(X)		((X) * (X))
 
-#define SAFE_DELETE_B(p)	if(p){\
-delete[]p;\
-p = nullptr;\
-}
+#define		SAFE_DELETE(p)			if(p){ 	delete p ;	p = nullptr; }
+#define		SAFE_DELETE_ARRAY(p)		if(p){ 	delete []p ;	p = nullptr; }
 
 // 조건부 컴파일
 
 using namespace std;
 
-int main() {
-	// char szName[MIN_STR] = "";
-	// cout << PI << endl;
+int main()
+{
+
+	// char	szName[MIN_STR] = "";
+	// cout << 3.14f << endl;
 
 	// cout << SQUARE(10) << endl;
 	// cout << SQUARE(11.1f) << endl;
-	// cout << SQUARE((2 + 2)) << endl;
 
-	int* p = new int;
-	cout << p << endl;
-	SAFE_DELETE(p);
-	cout << p << endl;
+	// cout << SQUARE(2 + 2) << endl;
+
+	int iInput(0);
+
+	cin >> iInput;
+
+	int* p = new int[iInput];
+
+	SAFE_DELETE_ARRAY(p);
+
+
+
+
 	return 0;
 }
