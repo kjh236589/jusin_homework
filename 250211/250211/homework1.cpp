5af15a560d40555482e376151a22adc19a43bc50
IBulan <40731551+IBulan@users.noreply.github.com>
2025-02-13 00:21:54 +0900

    숙제 완료

--- 250211/250211/homework1.cpp
+++ 250211/250211/homework1.cpp
@@ -1,6 +1,213 @@
 #include "pch.h"
 
+class Object
+{
+public:
+	char szName[32];
+	int iMaxHp;
+	int iHp;
+	int iAtk;
+
+	void CreateObject(char szname[32], int imaxhp, int iatk) {
+		strcpy_s(szName, sizeof(szName), szname);
+		iMaxHp = imaxhp;
+		iAtk = iatk;
+		Reset();
+	}
+
+	void SetObject(char szname[32], int imaxhp,  int ihp, int iatk) {
+		strcpy_s(szName, sizeof(szName), szname);
+		iMaxHp = imaxhp;
+		iHp = ihp;
+		iAtk = iatk;
+	}
+
+	void Reset() {
+		iHp = iMaxHp;
+	}
+
+	void Damage(Object oObject) {
+		iHp -= oObject.iAtk;
+	}
+};
+
+Object* CreatePlayer();
+
+void Print(Object* oObject);
+
+void PlayGame(Object* oPlayer);
+
+void Choise(Object* oPlayer);
+
+Object* CreateMonster(int iInput);
+
+void Fight(Object* oPlayer, Object* oMonster);
+
 int main()
 {
+	Object* oPlayer = CreatePlayer();
+	if ((*oPlayer).iHp != 0) {
+		PlayGame(oPlayer);
+	}
+	cout << "게임 종료" << endl;
+	FILE* pSaveFile = NULL;
+	errno_t errSave = fopen_s(&pSaveFile, "../Data/Save.dat", "wb");
+	if (0 == errSave)
+	{
+		Object Save[1] = { *oPlayer };
+		fwrite(Save, sizeof(Save), 1, pSaveFile);
+	}
+	else {
+		cout << "세이브 실패" << endl;
+		system("pause");
+	}
+	fclose(pSaveFile);
+	delete oPlayer;
+	oPlayer = nullptr;
 	return 0;
+}
+
+Object* CreatePlayer() {
+	Object* oPlayer = new Object();
+	int iInput(0);
+	char szName[32] = {};
+	Object oSave[1] = {};
+	FILE* pLoadSave = NULL;
+	errno_t errLoad = fopen_s(&pLoadSave, "../Data/Save.dat", "rb");
+	while (true)
+	{
+		system("cls");
+		cout << "직업을 선택하세요(1. 전사 2. 마법사 3. 도적 4. 불러오기 5. 나가기) : ";
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			strcpy_s(szName, sizeof(szName), "전사");
+			oPlayer->CreateObject(szName, 100, 10);
+			break;
+		case 2:
+			strcpy_s(szName, sizeof(szName), "마법사");
+			oPlayer->CreateObject(szName, 100, 10);
+			break;
+		case 3:
+			strcpy_s(szName, sizeof(szName), "도적");
+			oPlayer->CreateObject(szName, 100, 10);
+			break;
+		case 4:
+			if (0 == errLoad)
+			{
+				fread(oSave, sizeof(oSave), 1, pLoadSave);
+				oPlayer->SetObject(oSave[0].szName, oSave[0].iMaxHp, oSave[0].iHp, oSave[0].iAtk);
+			}
+			else {
+				cout << "세이브 로드 실패" << endl;
+			}
+			break;
+		case 5:
+			break;
+		default:
+			cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+			continue;
+		}
+		fclose(pLoadSave);
+		return oPlayer;
+	}
+}
+
+void Print(Object* oObject) {
+	cout << "==================================" << endl;
+	cout << "이름 : " << oObject->szName << endl;
+	cout << "체력 : " << oObject->iHp << "\t공격력 : " << oObject->iAtk << endl;
+}
+
+void PlayGame(Object* oPlayer) {
+	int iInput(0);
+	while (true)
+	{
+		system("cls");
+		Print(oPlayer);
+		cout << "1. 사냥터 2. 종료 : ";
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			Choise(oPlayer);
+			break;
+		case 2:
+			return;
+		}
+	}
+}
+
+void Choise(Object* oPlayer) {
+	int iInput(0);
+	Object* oMonster;
+	while (true)
+	{
+		system("cls");
+		Print(oPlayer);
+		cout << "1. 초급 2. 중급 3. 고급 4. 전 단계 : ";
+		cin >> iInput;
+		if (0 < iInput && 3 >= iInput) {
+			oMonster = CreateMonster(iInput);
+			Fight(oPlayer, oMonster);
+			delete oMonster;
+			oMonster = nullptr;
+		}
+		else if (iInput == 4) {
+			return;
+		}
+	}
+}
+
+Object* CreateMonster(int iInput) {
+	char szName[32] = {};
+	Object* oMonster = new Object();
+	switch (iInput)
+	{
+	case 1:
+		strcpy_s(szName, sizeof(szName), "초급");
+		oMonster->CreateObject(szName, 30, 3);
+		break;
+	case 2:
+		strcpy_s(szName, sizeof(szName), "중급");
+		oMonster->CreateObject(szName, 60, 6);
+		break;
+	case 3:
+		strcpy_s(szName, sizeof(szName), "고급");
+		oMonster->CreateObject(szName, 90, 9);
+		break;
+	}
+	return oMonster;
+}
+
+void Fight(Object* oPlayer, Object* oMonster) {
+	int iInput(0);
+	while (1) {
+		system("cls");
+		Print(oPlayer);
+		Print(oMonster);
+		cout << "1. 공격 2. 도망 : ";
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			oPlayer->Damage(*oMonster);
+			oMonster->Damage(*oPlayer);
+			if (0 >= oPlayer->iHp) {
+				cout << "플레이어 사망" << endl;
+				oPlayer->Reset();
+				system("pause");
+			}
+			else if (0 >= oMonster->iHp) {
+				cout << "승리" << endl;
+				system("pause");
+			}
+			else {
+				break;
+			}
+		case 2:
+			return;
+		}
+	}
 }
--- 250211/250211/homework2.cpp
+++ 250211/250211/homework2.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
 #include "pch.h"
 
 int main()
 {
+	FILE* pLoadImage = NULL;
+	FILE* pCopyImage = NULL;
+	errno_t errLoad = fopen_s(&pLoadImage, "../Data/Image.jpg", "rb");
+	if (0 == errLoad)
+	{
+		cout << "이미지 로드 성공" << endl;
+		errno_t errSave = fopen_s(&pCopyImage, "../Data/Image_복사.jpg", "wb");
+		if (0 == errSave)
+		{
+			cout << "이미지 복사 성공" << endl;
+			char image[1] = {};
+			while (0 == feof(pLoadImage))
+			{
+				image[0] = fgetc(pLoadImage);
+				fwrite(image, sizeof(image), 1, pCopyImage);
+			}
+		}
+		else
+			cout << "복사 실패" << endl;
+		fclose(pCopyImage);
+	}
+	else
+		cout << "이미지 로드 실패" << endl;
+	fclose(pLoadImage);
 	return 0;
 }
