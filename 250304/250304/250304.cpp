49740309af879edc40824abf921bb0d8e5919008
IBulan <40731551+IBulan@users.noreply.github.com>
2025-03-04 15:37:57 +0900

    250304

---
+++ 250304/250304/250304.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+
+int main()
+{
+	vector<int> vecInt;
+	vecInt.push_back(10);
+	vecInt.push_back(20);
+	vecInt.push_back(30);
+	vecInt.push_back(40);
+	for (size_t i = 0; i < vecInt.size(); ++i) {
+		cout << vecInt[i] << endl;
+	}
+	return 0;
+}
